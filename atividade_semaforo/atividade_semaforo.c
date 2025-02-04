f7e606f79349e0a624120a046743b59ee8f329de
ArilsonOliveira <arilsonoliveira1798@gmail.com>
2025-02-04 16:34:07 -0300

    Codigo do semaforo em funcionamento

--- atividade_semaforo/atividade_semaforo.c
+++ atividade_semaforo/atividade_semaforo.c
@@ -1,14 +1,60 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
 
+#define LED_RED 13
+#define LED_YELLOW 12 // converter o azul para amarelo
+#define LED_GREEN 11
 
+volatile int state = 0;
 
-int main()
-{
+bool repeating_timer_callback(struct repeating_timer *t) {
+    switch (state) {
+        case 0: // Vermelho -> Amarelo
+        
+        printf("Sinal amarelo. Atenção. \n");
+            gpio_put(LED_RED, 0);
+            gpio_put(LED_YELLOW, 1);
+            state = 1;
+            break;
+        case 1: // Amarelo -> Verde
+        printf("Sinal verde. Prossiga, caminho livre.\n");
+            gpio_put(LED_YELLOW, 0);
+            gpio_put(LED_GREEN, 1);
+            state = 2;
+            break;
+        case 2: // Verde -> Vermelho
+        
+        printf("Sinal vermelho. Pare. \n");
+            gpio_put(LED_GREEN, 0);
+            gpio_put(LED_RED, 1);
+            state = 0;
+            break;
+    }
+    return true; // Continua repetindo
+}
+
+int main() {
     stdio_init_all();
-
+    
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+    gpio_init(LED_YELLOW);
+    gpio_set_dir(LED_YELLOW, GPIO_OUT);
+    gpio_init(LED_GREEN);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    
+    // Inicialização do semáforo na cor vermelha
+    gpio_put(LED_RED, 1);
+    gpio_put(LED_YELLOW, 0);
+    gpio_put(LED_GREEN, 0);
+    
+    struct repeating_timer timer;
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
+    
     while (true) {
-        printf("Hello, world!\n");
+        printf("Semáforo funcionando...\n");
         sleep_ms(1000);
     }
 }
+
